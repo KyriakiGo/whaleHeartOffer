724e0314d2f3ca7796b92c39b29a88a30fcc50b5
Kyriaki Goni <g.kyriaki@gmail.com>
2024-11-14 20:09:11 +0200

    Update javascript.js

--- javascript.js
+++ javascript.js
@@ -1,108 +1,18 @@
-// function webgl_detect(return_context)
-// {
-//     if (!!window.WebGLRenderingContext) {
-//         var canvas = document.createElement("canvas"),
-//              names = ["webgl2", "webgl", "experimental-webgl", "moz-webgl", "webkit-3d"],
-//            context = false;
-//
-//         for(var i=0;i< names.length;i++) {
-//             try {
-//                 context = canvas.getContext(names[i]);
-//                 if (context && typeof context.getParameter == "function") {
-//                     // WebGL is enabled
-//                     if (return_context) {
-//                         // return WebGL object if the function's argument is present
-//                         return {name:names[i], gl:context};
-//                     }
-//                     // else, return just true
-//                     return true;
-//                 }
-//             } catch(e) {}
-//         }
-//
-//         // WebGL is supported, but disabled
-//         return false;
-//     }
-//
-//     // WebGL not supported
-//     return false;
-// }
+document.addEventListener('DOMContentLoaded', () => {
+  // Set event listener for the first tap or click
+  document.addEventListener('click', startModelDisplay, { once: true });
+});
 
-AFRAME.registerComponent('model-opacity', {
-  schema: {default: 1.0},
-  init: function () {
-    this.el.addEventListener('model-loaded', this.update.bind(this));
-  },
-  update: function () {
-    var mesh = this.el.getObject3D('mesh');
-    var data = this.data;
-    if (!mesh) { return; }
-    mesh.traverse(function (node) {
-      if (node.isMesh) {
-        node.material.opacity = data;
-        node.material.transparent = data < 1.0;
-        node.material.needsUpdate = true;
-      }
-    });
+function startModelDisplay() {
+  // Display the 3D model in AR after the first user interaction
+  const modelEntity = document.querySelector('#modelEntity');
+  if (modelEntity) {
+    modelEntity.setAttribute('visible', 'true'); // Show the 3D model
   }
-});
-
-AFRAME.registerComponent('fade-time', {
-  schema: {default: 1000},
-});
-
-
-// Setup for a-frame
-AFRAME.registerComponent('samplehandler', {
-    init: function () {
-        console.log("samplehandler init...")
-        let marker = this.el;
-        //let marker = this.el.sceneEl;
-
-        marker.addEventListener('markerFound', function () {
-          //if
-          var text = document.querySelector("#text");
-            var model = document.querySelector("#flower");
-            var opacity = model.getAttribute('model-opacity');
-            var fade = model.getAttribute('fade-time');
-            console.log("Opacity "+opacity);
-            if(opacity==0){
-            // if(model)
-            console.log("markerFound...");
-            // var model = document.querySelector("#flower");
-            text.emit('fade-in');
-            model.emit('fade-in')
-            console.log("visiblity  true");
-            setTimeout(() => {
-            model.emit('fade-out')
-            text.emit('fade-out');
-  console.log("visiblity  false");
-}, fade)
-};
-console.clear()
-        }.bind(this));
-
-        marker.addEventListener('markerLost', function() {
-            console.log("markerLost...");
-            console.clear()
-        }.bind(this));
-    }
-});
-
-
-
-
-window.onload = function() {
-    console.log("window loaded...");
-};
-
-
-
-// var m = document.querySelector("a-nft")
-// m.addEventListener("markerFound", (e)=>{
-//    console.log("found")
-// })
-//
-// m.addEventListener("markerLost", (e)=>{
-//    console.log("lost")
-// })
+  
+  // Optional: Start sound if available
+  const soundEl = document.querySelector('#ambientSound');
+  if (soundEl && soundEl.components.sound) {
+    soundEl.components.sound.playSound();
+  }
+}
